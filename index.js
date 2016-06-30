6d09f8166683a489bb710a9f507800847a92af8b
Taras Mankovski <tarasm@gmail.com>
2016-06-30 18:40:32 -0400

    Adding dependency to vendor

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
+var dirname = path.dirname;
+var join = path.join;
+var resolve = path.resolve;
+var mergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-element-resize-detector',
-  options: {
-    nodeAssets: {
-      'element-resize-detector': {
-        srcDir: 'dist',
-        import: ['element-resize-detector.js']
-      }
+
+  included: function(app) {
+    if (app.app) {
+      app = app.app;
     }
+    this.app = app;
+
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      app.import('vendor/element-resize-detector/element-resize-detector.js');
+    }
+  },
+
+  treeForVendor(tree) {
+    var erdSrc = dirname(require.resolve('element-resize-detector'));
+    var erdPath = resolve(join(erdSrc, '..', 'dist'));
+    var erdTree = new Funnel(this.treeGenerator(erdPath), {
+      srcDir: '/',
+      files: ['element-resize-detector.js'],
+      destDir: 'element-resize-detector'
+    });
+    return mergeTrees([tree, erdTree]);
   }
 };
